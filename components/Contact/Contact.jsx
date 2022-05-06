dbb60a8b4436b2fca8f3c96c0a8c30c46b68f688
Diego <diegojairhl@gmail.com>
2022-05-05 21:39:14 -0500

    contact component with body response sent to gmail, thank-you page and 404 page implemented

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,29 +1,167 @@
+import axios from "axios";
+import { useState, useRef } from "react";
+import { useRouter } from "next/router";
+
+import ReCAPTCHA from "react-google-recaptcha";
+import { ErrorMessage } from "@hookform/error-message";
+import { useForm } from "react-hook-form";
+
 import Lottie from "lottie-react";
-import Section from "../Section";
 import animConnect from "../../src/lotties/connect-lottie.json";
-// import { useRef } from "react";
+
+import Section from "../Section";
 
 const Contact = () => {
-  // const lottieRef = useRef();
+  const router = useRouter();
+
+  const captcha = useRef(null);
+
+  const [verified, setVerified] = useState(false);
+  const [validCaptcha, setValidCaptcha] = useState(null);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmitForm = async (values) => {
+    if (!captcha.current.getValue()) return setValidCaptcha(false);
+
+    const config = {
+      method: "post",
+      url: `${process.env.NEXT_PUBLIC_API_URL}/api/contact`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: values,
+    };
+
+    try {
+      const response = await axios(config);
+      if (response.status === 200) {
+        captcha.current.reset();
+        reset();
+        setVerified(false);
+        router.push("/thank-you");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setValidCaptcha(null);
+  };
+
+  const onChange = () => {
+    if (captcha.current.getValue()) {
+      setVerified(true);
+      setValidCaptcha(true);
+    }
+  };
   return (
     <Section className="section contact">
       <figure className="contact__img">
         <Lottie animationData={animConnect} autoplay={true} loop={true} />
       </figure>
-      <form action="/" className="form" id="form">
+      <form action="/" className="form" id="form" onSubmit={handleSubmit(onSubmitForm)}>
         <h3 className="form__title">Póngase en contacto para nuevas oportunidades o solo para saludar :)</h3>
         <div className="form__input-list">
-          <input className="form__input" placeholder="Nombre" type="text" id="nombre" required />
-          <input className="form__input" placeholder="Email" type="email" name="email" id="email" required />
+          <label htmlFor="name" className="sr-only">
+            Nombre Completo
+          </label>
           <input
-            className="form__input form__input--message"
-            placeholder="Escribe el mensaje aquí."
+            className={`form__input ${errors.name ? "border-red-400" : "border-[#424242]"}`}
+            placeholder="Nombre completo"
+            {...register("name", {
+              required: {
+                value: true,
+                message: "Ingresa tu nombre completo porfavor",
+              },
+            })}
             type="text"
+            id="name"
+            name="name"
+          />
+          <ErrorMessage
+            errors={errors}
+            name="name"
+            render={({ message }) => <p className="text-red-400 text-sm py-1">{message}</p>}
+          />
+
+          <label htmlFor="email" className="sr-only">
+            Email
+          </label>
+          <input
+            className={`form__input ${errors.email ? "border-red-400" : "border-[#424242]"}`}
+            placeholder="Email"
+            {...register("email", {
+              required: {
+                value: true,
+                message: "Ingresa tu email por favor",
+              },
+              maxLength: {
+                value: 80,
+                message: "This is too long",
+              },
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i,
+                message: "Por favor ingresa un email válido",
+              },
+            })}
+            name="email"
+            type="text"
+            id="email"
+          />
+          <ErrorMessage
+            errors={errors}
+            name="email"
+            render={({ message }) => <p className="text-red-400 text-sm py-1">{message}</p>}
+          />
+
+          <label htmlFor="message" className="sr-only">
+            Message
+          </label>
+          <textarea
+            className={`form__input form__input--message  ${errors.message ? "border-red-400" : "border-[#424242]"}`}
+            placeholder="Escribe el mensaje aquí."
+            {...register("message", {
+              required: {
+                value: true,
+                message: "Ingresa el mensaje por favor",
+              },
+              maxLength: {
+                value: 1000,
+                message: "Your message can't be more than 1000 characters",
+              },
+            })}
+            name="message"
             id="message"
-            required
+          />
+          <ErrorMessage
+            errors={errors}
+            name="message"
+            render={({ message }) => <p className="text-red-400 text-sm py-1 ">{message}</p>}
           />
         </div>
-        <button className="btn" type="submit">
+        <div className="flex justify-center flex-col gap-2">
+          <ReCAPTCHA
+            onChange={onChange}
+            ref={captcha}
+            className="mx-auto"
+            sitekey="6LfzfckfAAAAAD92GvNz-tTfQCtBBF2kUc0wG8S9"
+          />
+          {validCaptcha === false && <p className="text-red-400 text-sm mx-auto">Por favor acepta el captcha</p>}
+        </div>
+        <button
+          className={`btn border-2 
+           w-fit mx-auto py-1 px-4 rounded-3xl  ${
+             verified
+               ? "hover:bg-gray-200 hover:shadow-xl   focus:shadow-xl  focus:bg-gray-200 border-black"
+               : "text-gray-400 border-gray-400"
+           }`}
+          type="submit"
+        >
           Enviar
         </button>
       </form>
---
+++ pages/thank-you.js
@@ -0,0 +1,47 @@
+import Lottie from "lottie-react";
+import AnimationTY from "../src/lotties/thankyou-lottie.json";
+import Layout from "../components/Layout.jsx";
+import Link from "next/link";
+
+import { useRef } from "react";
+import Section from "../components/Section";
+import tw from "twin.macro";
+
+const ThankYouPage = () => {
+  const style = {
+    // width: 100,
+    // height: 30,
+  };
+  const lottieRef = useRef();
+  return (
+    <Layout>
+      <ThankYouSection>
+        <Lottie
+          animationData={AnimationTY}
+          lottieRef={lottieRef}
+          style={style}
+          loop={true}
+          autoplay={true}
+          className="w-[300px] md:w-[500px] mx-auto"
+        />
+        <div className="p-5 flex flex-col items-center justify-center h-full gap-4">
+          <h1 className="text-5xl md:text-6xl font-semibold tracking-tighter text-center ">Gracias por tu mensaje!</h1>
+          <p className="mb-4 text-lg text-gray-500 text-center tracking-tighter leading-5 ">
+            Me comunicaré contigo lo más antes posible. :)
+          </p>
+          <Link href="/" passHref>
+            <a className="w-fit py-1 px-4 rounded-full border border-gray-900 bg-black text-[#ececec] hover:bg-opacity-80 hover:shadow-xl transition-colors">
+              Regresar a Home
+            </a>
+          </Link>
+        </div>
+      </ThankYouSection>
+    </Layout>
+  );
+};
+
+const ThankYouSection = tw(Section)`
+grid grid-cols-1 md:grid-cols-2 pt-0
+`;
+
+export default ThankYouPage;
