1792aa99608d2f65af2eb34e88d14c48ece25e78
Diego <diegojairhl@gmail.com>
2022-05-11 22:28:11 -0500

    Navbar menu styles update and state on mobile

--- components/Buttons/StandarButton.jsx
+++ components/Buttons/StandarButton.jsx
@@ -2,7 +2,9 @@ import tw from "twin.macro";
 
 const StandardButton = ({ text, color }) => {
   return (
-    <p className="w-fit py-1 px-4 rounded-full border border-gray-900 hover:bg-black hover:text-[#ececec]">{text}</p>
+    <p className="w-fit py-1 px-4 rounded-full border border-gray-900 hover:bg-black hover:text-[#ececec] transition-all">
+      {text}
+    </p>
   );
 };
 
