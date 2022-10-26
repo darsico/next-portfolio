0561e45b37ae7e74179504a0fb37b5c046c4d148
Diego <diegojairhl@gmail.com>
2022-10-26 14:40:38 -0500

    [HOTFIX] mobile hamburger menu changed to plain css

--- components/Navbar/MenuToggleButton.jsx
+++ components/Navbar/MenuToggleButton.jsx
@@ -1,34 +1,12 @@
-import Lottie from "lottie-react";
-import { useEffect, useRef } from "react";
-import Hamburger from "../../src/lotties/menu-portfolio.json";
-
-const style = {
-  width: 30,
-};
-
 const MenuToggleButton = ({ setOpen, open }) => {
-  const lottieRef = useRef();
   const handleButtonClick = () => {
     setOpen(!open);
   };
 
-  useEffect(() => {
-    if (open) {
-      lottieRef.current.playSegments([0, 40], true);
-    } else {
-      lottieRef.current.playSegments([10, 0], true);
-    }
-  }, [open]);
-
   return (
-    <Lottie
-      onClick={handleButtonClick}
-      className="z-50 block cursor-pointer md:hidden"
-      animationData={Hamburger}
-      lottieRef={lottieRef}
-      style={style}
-      autoplay={false}
-    />
+    <div class={`menu-btn-1 ${open ? 'active' : ''} z-50 cursor-pointer md:hidden  flex justify-end `} onClick={handleButtonClick}>
+      <span></span>
+    </div>
   );
 };
 
