7374bcc49ba4d6aba1c257eabbf2ea3e3cea9a46
Diego <diegojairhl@gmail.com>
2022-11-11 20:56:41 -0500

    update: dynamic lang in footbar component

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,20 +1,24 @@
+import { useContext } from "react";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { FaHeart } from "react-icons/fa";
+import { LanguageContext } from "../../context/LanguageContext";
 import Section from "../Section";
 
 const Footer = () => {
+  const { dictionary } = useContext(LanguageContext)
+  const { followMe, checkThis, projects, aboutMe, resume, madeWith, by, textMe } = dictionary
   return (
     <footer className="footer">
       <FooterSection>
         <ul className="footer-list">
           <li className="footer-list__item">
-            <h5 className="footer-list__title">Escríbeme</h5>
+            <h5 className="footer-list__title">{textMe}</h5>
             <a href="mailto:diegojairhl@gmail.com" target="_blank" rel="noreferrer">
               diegojairhl@gmail.com
             </a>
           </li>
           <li className="footer-list__item footer-list__item--center">
-            <h5 className="footer-list__title">Sígueme</h5>
+            <h5 className="footer-list__title">{followMe}</h5>
             <ul className="footer__list-icons">
               <li>
                 <a href="https://github.com/darsico" target="_blank" rel="noreferrer">
@@ -30,25 +34,25 @@ const Footer = () => {
             </ul>
           </li>
           <li className="md:ml-auto footer-list__item">
-            <h5 className="footer-list__title">Checa esto</h5>
+            <h5 className="footer-list__title">{checkThis}</h5>
             <ul className="footer-list__item">
               <li>
-                <a href="#work">Proyectos</a>
+                <a href="#work">{projects}</a>
               </li>
               <li>
-                <a href="">Sobre mi</a>
+                <a href="">{aboutMe}</a>
               </li>
               <li>
-                <a href="">Mi CV</a>
+                <a href="">{resume}</a>
               </li>
             </ul>
           </li>
         </ul>
       </FooterSection>
       <h3 className="signature">
-        Hecho con
+        {madeWith}
         <FaHeart className="signature__icon heart" alt="" />
-        por
+        {by}
         <span className="signature__name"> Diego Huaman</span>
       </h3>
     </footer>
