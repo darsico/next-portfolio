f65554edf04e03b9dab8be9cc64b6923cbf1f06e
Diego <diegojairhl@gmail.com>
2022-11-12 21:41:11 -0500

    update: option show only two first chars

--- components/Navbar/LangSelect.jsx
+++ components/Navbar/LangSelect.jsx
@@ -1,17 +1,10 @@
 import { languageOptions } from "../../src/utils/language";
 import { useLanguageStore } from "../../store/store";
-import { useEffect, useState } from "react";
 
 const LangSelect = () => {
  const { userLanguage, userLanguageChange } = useLanguageStore((state) => state)
- const [mobileScreen, setMobileScreen] = useState();
 
  const handleLanguageChange = e => userLanguageChange(e.target.value);
-
- useEffect(() => {
-  const screen = window.innerWidth < 768;
-  setMobileScreen(screen);
- }, [])
 
  return (<select
   onChange={handleLanguageChange}
@@ -19,8 +12,7 @@ const LangSelect = () => {
   className="cursor-pointer bg-transparent focus:border-transparent"
  >
   {Object.entries(languageOptions).map(([id, name]) => {
-   console.log("id:", id, "name", name)
-   return <option key={id} value={id}>{mobileScreen ? `${name.slice(0, 2).toUpperCase()}` : name}</option>
+   return <option key={id} value={id}>{name.slice(0, 2).toUpperCase()}</option>
   })}
  </select>);
 }
